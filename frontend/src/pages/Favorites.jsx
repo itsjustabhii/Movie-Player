e61fa6369d31800de12f6955712f2e8b8894ca0d
Abhishek Harish <itsjustabhii@gmail.com>
2026-02-07 10:08:56 +0530

    added favorite code

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import '../css/Favorites.css'
+import { useMovieContext } from '../contexts/MovieContext'
+import MovieCard from '../components/MovieCard'
 
 const Favorites = () => {
+  const {favorites} = useMovieContext()
+
+  if(Favorites){
+    return( 
+      <div className='favorites'>
+        <h2>Your Favorites</h2>
+    <div className='mvoies-grid'>
+      {favorites.map((movie) =>(
+        <MovieCard movie={movie.id} key={movie}/>
+      ))}
+    </div>
+    </div>)
+  }
   return (
     <div className='favorites-empty'>
         <h2>No favorite movies</h2>
