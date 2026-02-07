771094670b04f6414b733117af49d9b2c8981967
Abhishek Harish <itsjustabhii@gmail.com>
2026-02-07 09:48:55 +0530

    added movie contextjsx file

---
+++ frontend/src/contexts/MovieContext.jsx
@@ -0,0 +1,44 @@
+import React, { Children } from 'react'
+import { createContext, useState, useContext, useEffect } from 'react'
+
+const MovieContext = createContext()
+
+export const useMovieContext = () => useContext(MovieContext)
+export const MovieProvider = ({children}) => {
+    const [favorites, setFavorites] = useState([])
+
+    useEffect(() => {
+        const storedFavs = localStorage.getItem("favorites")
+
+        if(storedFavs) setFavorites(JSON.parse(storedFavs))
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("favorites", JSON.stringify(favorites))
+    },[favorites])
+
+    const addToFavorites = (moovie) =>{
+        setFavorites(prev => [...prev, moovie])
+    }
+
+    const removeFromFavorites = (movieId) =>{
+        setFavorites(prev => prev.filter(movie => movie.id !== movieId))
+    }
+
+    const isFavorite = (movieId) => {
+        return favorites.some(movie => movie.id === movieId)
+    }
+
+    const value ={
+        favorites,
+        addToFavorites,
+        removeFromFavorites,
+        isFavorite
+    }
+
+    return <MovieContext.Provider value={value}>
+        {children}
+    </MovieContext.Provider>
+}
+
+export default MovieContext
